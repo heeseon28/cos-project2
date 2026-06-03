76a349d1cd6c0fd18136095b97f4e9555a341340
heeseon28 <hgbell28@gmail.com>
2026-06-03 14:53:55 +0900

    feat::bracnh 분리

--- edge/main.cpp
+++ edge/main.cpp
@@ -19,7 +19,7 @@ void usage(uint8_t *pname)
 
 int main(int argc, char *argv[])
 {
-	int c, tmp, port;
+  int c, tmp, port;
   uint8_t *pname, *addr;
   uint8_t eflag;
   Edge *edge;
@@ -33,10 +33,9 @@ int main(int argc, char *argv[])
   {
     int option_index = 0;
     static struct option long_options[] = {
-      {"addr", required_argument, 0, 'a'},
-      {"port", required_argument, 0, 'p'},
-      {0, 0, 0, 0}
-    };
+        {"addr", required_argument, 0, 'a'},
+        {"port", required_argument, 0, 'p'},
+        {0, 0, 0, 0}};
 
     const char *opt = "a:p:0";
 
@@ -47,18 +46,18 @@ int main(int argc, char *argv[])
 
     switch (c)
     {
-      case 'a':
-        tmp = strlen(optarg);
-        addr = (uint8_t *)malloc(tmp);
-        memcpy(addr, optarg, tmp);
-        break;
+    case 'a':
+      tmp = strlen(optarg);
+      addr = (uint8_t *)malloc(tmp);
+      memcpy(addr, optarg, tmp);
+      break;
 
-      case 'p':
-        port = atoi(optarg);
-        break;
+    case 'p':
+      port = atoi(optarg);
+      break;
 
-      default:
-        usage(pname);
+    default:
+      usage(pname);
     }
   }
 
@@ -84,5 +83,5 @@ int main(int argc, char *argv[])
   edge->init();
   edge->run();
 
-	return 0;
+  return 0;
 }
